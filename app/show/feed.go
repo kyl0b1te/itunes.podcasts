0321f8830b0d9e2dac6213f70ed42e52e1ab9026
zhikiri <zhikiri.dev@gmail.com>
2019-08-28 17:23:21 +0300

    complement feed with language

--- app/show/feed.go
+++ app/show/feed.go
@@ -2,12 +2,16 @@ package show
 
 import (
 	"encoding/xml"
+	"encoding/json"
+	"strings"
 	"errors"
 	"github.com/zhikiri/uaitunes-podcasts/app/crawler"
+	"github.com/zhikiri/uaitunes-podcasts/app/static"
 )
 
 type Feed struct {
 	ID          int
+	Language    string
 	Description string
 	LastPodcast Podcast
 }
@@ -23,6 +27,7 @@ type RSS struct {
 	Channel struct {
 		XMLName       xml.Name `xml:"channel"`
 		Description   string   `xml:"description"`
+		Language      string   `xml:"language"`
 		LastBuildDate string   `xml:"lastBuildDate"`
 		Item          struct {
 			Title       string `xml:"title"`
@@ -35,22 +40,32 @@ func GetFeed(shows []*ShowDetails) ([]*Feed, []error) {
 
 	urlToID := getShowsURLToID(shows)
 
-	entities, errs := crawler.RequestEntities(
-		getRequestOptions(shows),
-		rssDecoder,
-	)
+	feedList := make([]*Feed, 0, len(shows))
+	errs := []error{}
 
-	feeds := []*Feed{}
-	for _, entity := range entities {
-		feed, err := getFeedData(entity, urlToID)
+	out := crawler.RequestEntities(getRequestOptions(shows), rssDecoder)
+	for entity := range out {
+		if entity.Error != nil {
+			errs = append(errs, entity.Error)
+			continue
+		}
+		feed, err := getFeedData(entity.Entity, entity.URL, urlToID)
 		if err != nil {
 			errs = append(errs, err)
 		} else {
-			feeds = append(feeds, feed)
+			feedList = append(feedList, feed)
 		}
 	}
 
-	return feeds, errs
+	return feedList, errs
+}
+
+func SaveFeed(path string, feed []*Feed) error {
+
+	return static.Save(path, func() ([]byte, error) {
+
+		return json.Marshal(feed)
+	})
 }
 
 func getShowsURLToID(shows []*ShowDetails) map[string]int {
@@ -87,16 +102,24 @@ func rssDecoder(url string, body []byte) (interface{}, error) {
 	return rss, nil
 }
 
-func getFeedData(entity interface{}, urlToID map[string]int) (*Feed, error) {
+func getFeedData(entity interface{}, url string, urlToID map[string]int) (*Feed, error) {
 
 	rss, ok := entity.(RSS)
 	if !ok {
 		return &Feed{}, errors.New("Invalid entity detected")
 	}
 
+	var id int
+	if id, ok = urlToID[url]; !ok {
+		return &Feed{}, errors.New("Cannot retrieve the show id")
+	}
+
+	lang := strings.Split(strings.ToLower(rss.Channel.Language), "-")[0]
+
 	return &Feed{
-		ID:          0,
+		ID:          id,
 		Description: rss.Channel.Description,
+		Language:    lang,
 		LastPodcast: Podcast{
 			Title:       rss.Channel.Item.Title,
 			Description: rss.Channel.Item.Description,
