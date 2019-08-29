f8b35e635b241217a36c456ba8a3f507e60198a2
zhikiri <zhikiri.dev@gmail.com>
2019-08-29 14:54:09 +0300

    started to add main package logic

--- app/main.go
+++ app/main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"log"
 
-	"github.com/zhikiri/uatines-podcasts/genre"
+	"github.com/zhikiri/uaitunes-podcasts/app/genre"
 )
 
 func main() {
-	fmt.Println("hello")
+
+	opt, err := newOptions()
+	stopOnError(err)
+
+	log.Println("--- Start ---")
+
+	switch *opt.Type {
+	case "genres":
+		log.Println("Parsing genres...")
+		genres, errs := genre.GetGenres(genre.GetRequestOptions())
+		stopOnErrors(errs)
+
+		log.Println(len(genres), "was successfully parsed")
+		genre.Save(*opt.Out, genres)
+	default:
+		//
+	}
+
+	log.Println("--- END ---")
+}
+
+func stopOnErrors(errs []error) {
+
+	if len(errs) == 0 {
+		return
+	}
+	for _, err := range errs {
+		fmt.Printf("[ERROR] %s\n", err)
+	}
+	os.Exit(1)
+}
+
+func stopOnError(err error) {
+
+	if err != nil {
+		fmt.Printf("[ERROR] %s\n", err)
+		os.Exit(1)
+	}
 }
